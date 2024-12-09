15c25fc415436e10f320f19d57a1328e81dedc1f
andy <andy@anonymouse.local>
2024-12-09 21:54:41 +0800

    adjust the example directory structure

--- app/routers/api.py
+++
@@ -1,9 +0,0 @@
-
-from fastapi import APIRouter
-from app.routers import users,roles
-
-api_router = APIRouter()
-
-api_router.include_router(roles.router,prefix="/roles")
-api_router.include_router(users.router,prefix="/company/{companyid}/users")
-
