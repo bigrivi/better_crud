8a60a378e99766990899770c96fad59ebff43dbb
bigrivi <sunjianghong@gmail.com>
2024-12-20 07:04:42 +0800

    some update

--- examples/sqlmodel/app/routers/roles.py
+++ examples/sqlmodel/app/routers/roles.py
@@ -1,13 +1,15 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter, Depends
 from fastapi_crud import crud
-from app.models.role import Role,RolePublic,RoleCreate,RoleUpdate
+from app.models.role import Role, RolePublic, RoleCreate, RoleUpdate
 from app.services.role import RoleService
 router = APIRouter()
 
-@crud(router,
-    dto={"create":RoleCreate,"update":RoleUpdate},
+
+@crud(
+    router,
+    dto={"create": RoleCreate, "update": RoleUpdate},
     routes={},
-    serialize={"base":RolePublic},
+    serialize={"base": RolePublic},
 )
 class RoleController():
     service: RoleService = Depends(RoleService)
--- examples/sqlmodel/app/routers/users.py
+++ examples/sqlmodel/app/routers/users.py
@@ -18,58 +18,59 @@ def filter_fn(request: Request):
     }
 
 
-@crud(router,
-      feature="user",
-      #   params={
-      #       "companyid": {
-      #           "field": "company_id",
-      #           "type": "str"
-      #       }
-      #   },
-      routes={
-          # "dependencies":[JWTDepend,ACLDepend],
-          # "only":["get_many","create_one"]
-          "get_many": {
-              "summary": ""
-          }
-      },
-      summary_vars={
-          "name": "sun"
-      },
-      dto={"create": UserCreate, "update": UserUpdate},
-      serialize={
-          "base": UserPublic,
-      },
-      auth={
-          # "filter":filter_fn
-      },
-      query={
-          "joins": {
-              #   "profile": {
-              #       "select": True,
-              #       "join": False
-              #   },
-              #   "tasks": {
-              #       "select": True,
-              #       "join": False
-              #   },
-              #   "company": {
-              #       "select": True,
-              #       "join": False
-              #   },
-              #   "roles": {
-              #       "select": True,
-              #       "join": False
-              #   }
-          },
-          "soft_delete": True,
-          "sort": [
-              {
-                  "field": "id",
-                  "sort": "DESC"
-              }
-          ]
-      }
-      )
+@crud(
+    router,
+    feature="user",
+    #   params={
+    #       "companyid": {
+    #           "field": "company_id",
+    #           "type": "str"
+    #       }
+    #   },
+    routes={
+        # "dependencies":[JWTDepend,ACLDepend],
+        # "only":["get_many","create_one"]
+        "get_many": {
+            "summary": ""
+        }
+    },
+    summary_vars={
+        "name": "sun"
+    },
+    dto={"create": UserCreate, "update": UserUpdate},
+    serialize={
+        "base": UserPublic,
+    },
+    auth={
+        # "filter":filter_fn
+    },
+    query={
+        "joins": {
+            #   "profile": {
+            #       "select": True,
+            #       "join": False
+            #   },
+            #   "tasks": {
+            #       "select": True,
+            #       "join": False
+            #   },
+            #   "company": {
+            #       "select": True,
+            #       "join": False
+            #   },
+            #   "roles": {
+            #       "select": True,
+            #       "join": False
+            #   }
+        },
+        "soft_delete": True,
+        "sort": [
+            {
+                "field": "id",
+                "sort": "DESC"
+            }
+        ]
+    }
+)
 class UserController():
     service: UserService = Depends(UserService)
