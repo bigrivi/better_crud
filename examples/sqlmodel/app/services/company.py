efce42d5ca0e604862acfc6cbb723281e0c78ede
andy <andy@anonymouse.local>
2024-12-21 09:51:21 +0800

    fix issues service not instanced

--- examples/sqlmodel/app/services/company.py
+++ examples/sqlmodel/app/services/company.py
@@ -5,3 +5,10 @@ from ..models.company import Company
 class CompanyService(SqlalchemyCrudService[Company]):
     def __init__(self):
         super().__init__(Company)
+
+    async def on_before_create(
+        self,
+        create_data: dict,
+        background_tasks=None
+    ) -> None:
+        print("on_before_create call")
