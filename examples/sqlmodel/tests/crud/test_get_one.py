95ec4c3f3f5ae4fc58e8af7d3cdefda8f768aa6e
bigrivi <sunjianghong@gmail.com>
2024-12-27 20:49:46 +0800

    add Tests that generate abnormal conditions

--- examples/sqlmodel/tests/crud/test_get_one.py
+++ examples/sqlmodel/tests/crud/test_get_one.py
@@ -1,4 +1,5 @@
 import pytest
+from fastapi_crud.exceptions import NotFoundException
 from app.services.user import UserService
 
 
@@ -12,3 +13,9 @@ async def test_get_one_successful(async_session, test_user_data, test_request, i
     assert fetched_record.is_active == test_user_data[0]["is_active"]
 
 
+@pytest.mark.asyncio
+async def test_get_one_non_existent_record(async_session, test_user_data, test_request, init_data):
+    user_service = UserService()
+    non_existent_id = 1000
+    with pytest.raises(NotFoundException):
+        await user_service.crud_get_one(test_request, non_existent_id, db_session=async_session)
