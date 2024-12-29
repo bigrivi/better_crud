51548a54b93a6c6fbc10594eb0e7690e894ae507
bigrivi <sunjianghong@gmail.com>
2024-12-29 10:34:15 +0800

    update test

---
+++ examples/sqlmodel/tests/data/user.py
@@ -0,0 +1,78 @@
+USER_DATA = [
+    {
+        "id": 1,
+        "email": "bob@gmail.com",
+        "user_name": "bob",
+        "password": "111111",
+        "is_active": True,
+        "is_superuser": True,
+        "company_id": 1,
+        "profile": {
+            "name": "bob",
+            "gender": "male",
+            "phone": "111111",
+            "birthdate": "2020-01-01",
+            "hobby": "music",
+            "state": "nice",
+            "country": "china",
+            "address": "anhui"
+        },
+        "staff": {
+            "name": "bob",
+            "position": "CEO",
+            "job_title": "The Chief Executive Officer"
+        },
+        "tasks": [
+            {
+                "status": "pending",
+                "description": "pending task"
+            },
+            {
+                "status": "inprogress",
+                "description": "inprogress task"
+            },
+            {
+                "status": "completed",
+                "description": "completed task"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "email": "alice@gmail.com",
+        "user_name": "alice",
+        "password": "111111",
+        "is_active": True,
+        "is_superuser": False,
+        "company_id": 2,
+        "profile": {
+            "name": "alice",
+            "gender": "female",
+            "phone": "111111",
+            "birthdate": "2020-01-01",
+            "hobby": "music",
+            "state": "nice",
+            "country": "china",
+            "address": "anhui"
+        },
+        "staff": {
+            "name": "alice",
+            "position": "CFO",
+            "job_title": "Chief Financial Officer"
+        },
+        "tasks": [
+            {
+                "status": "pending",
+                "description": "pending task"
+            },
+            {
+                "status": "inprogress",
+                "description": "inprogress task"
+            },
+            {
+                "status": "completed",
+                "description": "completed task"
+            }
+        ]
+    }
+]
