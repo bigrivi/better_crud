089b7666f55676875a958bb65d02e562b63b00b6
bigrivi <sunjianghong@gmail.com>
2025-01-05 11:19:14 +0800

    update test

--- examples/sqlmodel/tests/crud/test_hooks.py
+++ examples/sqlmodel/tests/crud/test_hooks.py
@@ -42,7 +42,8 @@ def entity_service() -> EntityService:
     service.on_before_create = MagicMock(
         return_value=async_return({"key": "mykey"}))
     service.on_after_create = MagicMock(return_value=async_return())
-    service.on_before_update = MagicMock(return_value=async_return())
+    service.on_before_update = MagicMock(
+        return_value=async_return({"key": "mykey"}))
     service.on_after_update = MagicMock(return_value=async_return())
     service.on_before_delete = MagicMock(return_value=async_return())
     service.on_after_delete = MagicMock(return_value=async_return())
@@ -58,6 +59,7 @@ async def test_hooks_create_call(async_session, test_request, entity_service):
     entity_service.on_before_create.assert_called()
     entity_service.on_before_create.assert_called_once_with(
         entity_create, background_tasks=None)
+    entity_service.on_after_create.assert_called()
     entity_service.on_after_create.assert_called_once_with(
         entity, background_tasks=None)
     stmt = select(Entity).where(Entity.name == entity_name)
@@ -69,14 +71,24 @@ async def test_hooks_create_call(async_session, test_request, entity_service):
 
 @pytest.mark.asyncio
 async def test_hooks_update_call(async_session, test_request, entity_service):
-    entity = Entity(name="entity name", description="entity description")
+    entity_name = "entity name"
+    entity = Entity(name=entity_name, description="entity description")
     async_session.add(entity)
     await async_session.commit()
-    update_data = EntityUpdate(
+    entity_update = EntityUpdate(
         name="updated entity name", description="updated entity description")
-    await entity_service.crud_update_one(test_request, entity.id, update_data, db_session=async_session)
+    entity = await entity_service.crud_update_one(test_request, entity.id, entity_update, db_session=async_session)
     entity_service.on_before_update.assert_called()
+    entity_service.on_before_update.assert_called_once_with(
+        entity, entity_update, background_tasks=None)
     entity_service.on_after_update.assert_called()
+    entity_service.on_after_update.assert_called_once_with(
+        entity, background_tasks=None)
+    stmt = select(Entity).where(Entity.id == entity.id)
+    result = await async_session.execute(stmt)
+    fetched_record: Entity = result.scalar_one_or_none()
+    assert fetched_record is not None
+    assert fetched_record.key == "mykey"
 
 
 @pytest.mark.asyncio
@@ -84,6 +96,10 @@ async def test_hooks_delete_call(async_session, test_request, entity_service):
     entity = Entity(name="entity name", description="entity description")
     async_session.add(entity)
     await async_session.commit()
-    await entity_service.crud_delete_many(test_request, [entity.id], False, db_session=async_session)
+    entities = await entity_service.crud_delete_many(test_request, [entity.id], False, db_session=async_session)
     entity_service.on_before_delete.assert_called()
+    entity_service.on_before_delete.assert_called_once_with(
+        entities, background_tasks=None)
     entity_service.on_after_delete.assert_called()
+    entity_service.on_after_delete.assert_called_once_with(
+        entities, background_tasks=None)
