74c6da2436c4f87c369d0372fba8c6af329aea7c
andy <andy@anonymouse.local>
2025-01-15 19:51:17 +0800

    update unit test

---
+++ examples/sqlmodel/tests/core/test_async_session.py
@@ -0,0 +1,74 @@
+import pytest
+from typing import AsyncGenerator
+from fastapi.testclient import TestClient
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi_crud import FastAPICrudGlobalConfig, crud
+from fastapi.testclient import TestClient
+from fastapi import FastAPI, Depends, APIRouter
+from examples.sqlmodel.tests.helper import setup_database
+from app.services.user import UserService
+from app.models.user import UserPublic
+
+
+@pytest.mark.asyncio
+async def test_gen_async_session():
+    async def async_session() -> AsyncGenerator[AsyncSession, None]:  # pragma: no cover
+        async with setup_database("sqlite+aiosqlite:///:memory:") as session:
+            yield session
+
+    app = FastAPI()
+    FastAPICrudGlobalConfig.init(
+        backend_config={
+            "sqlalchemy": {
+                "db_session": async_session
+            }
+        }
+    )
+    user_router = APIRouter()
+
+    @crud(
+        user_router,
+        feature="user",
+        serialize={
+            "base": UserPublic,
+        }
+    )
+    class UserController():
+        service: UserService = Depends(UserService)
+    api_router = APIRouter()
+    api_router.include_router(user_router, prefix="/user")
+    app.include_router(api_router)
+    with TestClient(app) as test_client:
+        response = test_client.get("/user")
+        data = response.json()
+        assert len(data) == 0
+
+
+@pytest.mark.asyncio
+async def test_async_session(async_session):
+    app = FastAPI()
+    FastAPICrudGlobalConfig.init(
+        backend_config={
+            "sqlalchemy": {
+                "db_session": lambda: async_session
+            }
+        }
+    )
+    user_router = APIRouter()
+
+    @crud(
+        user_router,
+        feature="user",
+        serialize={
+            "base": UserPublic,
+        }
+    )
+    class UserController():
+        service: UserService = Depends(UserService)
+    api_router = APIRouter()
+    api_router.include_router(user_router, prefix="/user")
+    app.include_router(api_router)
+    with TestClient(app) as test_client:
+        response = test_client.get("/user")
+        data = response.json()
+        assert len(data) == 0
---
+++ examples/sqlmodel/tests/helper.py
@@ -0,0 +1,21 @@
+from sqlmodel import SQLModel
+from sqlalchemy.orm import sessionmaker
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator, List, Dict
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+
+@asynccontextmanager
+async def setup_database(url: str) -> AsyncGenerator[AsyncSession, None]:
+    engine = create_async_engine(url, echo=False, future=True)
+    session_maker = sessionmaker(
+        engine, class_=AsyncSession, expire_on_commit=False)
+    async with session_maker() as session:
+        async with engine.begin() as conn:
+            await conn.run_sync(SQLModel.metadata.create_all)
+        try:
+            yield session
+        finally:
+            async with engine.begin() as conn:
+                await conn.run_sync(SQLModel.metadata.drop_all)
+    await engine.dispose()
